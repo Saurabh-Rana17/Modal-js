998eb08fc7b047383c18133db5c296b427f249d6
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-09-18 14:25:29 +0530

    refactor

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let modal = document.querySelector(".modal");
 let overlay = document.querySelector(".overlay");
 let btnCloseModal = document.querySelector(".close-modal");
 let btnsOpenModal = document.querySelectorAll(".show-modal");
+let closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", () => {
-    modal.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
 }
-btnCloseModal.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
-  overlay.classList.toggle("hidden");
-});
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
