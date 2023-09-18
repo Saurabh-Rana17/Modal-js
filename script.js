850c7b52da46723da5cddfdf742cba0aa012ad15
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-09-18 13:21:47 +0530

    init

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let btnCloseModal = document.querySelector(".close-modal");
+let btnsOpenModal = document.querySelectorAll(".show-modal");
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", () => {
+    btnsOpenModal[i].textContent = "new value";
+  });
+}
