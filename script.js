2d0ad79ecd7d520c616bed7f2ce6c43ebad7b121
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-09-18 13:45:10 +0530

    toggle added

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let btnCloseModal = document.querySelector(".close-modal");
 let btnsOpenModal = document.querySelectorAll(".show-modal");
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", () => {
-    btnsOpenModal[i].textContent = "new value";
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
   });
 }
+btnCloseModal.addEventListener("click", () => {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+});
